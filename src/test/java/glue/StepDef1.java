d4ea1adb9770b8b98766c3af53ea128b482412d8
sanjay1991 <102404669+sanjay1991@users.noreply.github.com>
2022-05-23 15:19:03 +0530

    Update StepDef1.java

--- src/test/java/glue/StepDef1.java
+++ src/test/java/glue/StepDef1.java
@@ -40,7 +40,6 @@ import io.cucumber.java.en.When;
 		    else
 		    {
 		    	System.out.println("Compose is not displayed");
-			System.out.println("Hello");
 		    	System.exit(0); //0 means forcibly
 		    }
 		}
